04f99c451e3448f37101b15023b0b6feba314169
Andrew S <saipeg@yandex.ru>
2025-01-13 11:08:44 +0300

    Added join

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,5 +6,12 @@ public class Main {
 
         MyThread thread1 = new MyThread("Tick", tickTock);
         MyThread thread2 = new MyThread("Tock", tickTock);
+
+        try {
+            thread1.thrd.join();
+            thread2.thrd.join();
+        } catch (InterruptedException exc) {
+            System.out.println("Stop main thread");
+        }
     }
 }
